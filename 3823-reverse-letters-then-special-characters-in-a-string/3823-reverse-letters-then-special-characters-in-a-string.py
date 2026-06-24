27da531c64e1daa4143036eb4aafdc300fd33249
kedar sutar <kedarsutar37@gmail.com>
2026-06-24 18:17:41 +0530

    Time: 3 ms (67.8%), Space: 19.3 MB (78.43%) - LeetHub

---
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.py
@@ -0,0 +1,43 @@
+class Solution:
+    def reverseByType(self, s: str) -> str:
+        t = "abcdefghijklmnopqrstuvwxyz"
+        x = "!@#$%^&*()"
+
+        start = 0
+        end = len(s)-1
+        s = list(s)
+        while(start<end):
+            if(s[start] in t and s[end] in t):
+                s[start],s[end] = s[end],s[start]
+
+                start+=1
+                end  -=1
+
+            elif(s[start] not in t):
+                start+=1
+
+            else:
+                end-=1
+        
+
+        start = 0
+        end = len(s)-1
+        while(start<end):
+            if(s[start] in x and s[end] in x):
+                 s[start],s[end] = s[end],s[start]
+
+                 start +=1
+                 end-=1
+
+            elif(s[start] not in x):
+                start+=1
+
+            else:
+                end-=1
+
+        return "".join(s)
+
+
+
+
+        
