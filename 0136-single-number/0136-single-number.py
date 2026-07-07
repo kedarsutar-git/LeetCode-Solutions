76607f532863f929a87d585bc204b5d0d47e1173
kedar sutar <kedarsutar37@gmail.com>
2026-07-07 21:21:20 +0530

    Time: 5 ms (34.82%), Space: 21.6 MB (10.46%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         ans = 0
@@ -5,6 +6,25 @@ class Solution:
         for num in nums:
             ans = ans ^ num
 
-        return ans     
+        return ans   
+
+        '''  
            
-        
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        Map = {}
+        for num in nums:
+            if num in Map:
+                Map[num]+=1
+
+            else:
+                Map[num]=1
+
+        ans = 0
+        for key,value in Map.items():
+            if(value==1):
+                ans = key
+
+        return ans 
+
+
