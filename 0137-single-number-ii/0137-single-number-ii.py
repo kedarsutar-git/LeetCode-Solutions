5f37777a14d61b08e8beca152c6e12763fe3a553
kedar sutar <kedarsutar37@gmail.com>
2026-07-08 17:52:45 +0530

    Time: 0 ms (100%), Space: 20.8 MB (27.39%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        Map = {}
+        for num in nums:
+            if num in Map:
+                Map[num]+=1
+
+            else:
+                Map[num]=1
+
+        ans = 0
+        for key,value in Map.items():
+            if(value==1):
+                ans = key
+
+        return ans 
+                            
+
+
+            
+        
