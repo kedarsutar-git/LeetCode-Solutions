1e30d11e71716cd35edbd2a175987e299a323aa8
kedar sutar <kedarsutar37@gmail.com>
2026-07-15 11:40:19 +0530

    Time: 1112 ms (5.06%), Space: 19.5 MB (36.84%) - LeetHub

---
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        count_map1 = {}
+        for num in nums:
+            if( num in count_map1):
+                count_map1[num] += 1
+
+            else:
+                count_map1[num]  = 1
+
+        count = 0
+
+        for i in range(len(nums)):
+            count_map2 = {}
+            for j in range(i,len(nums)):
+                
+                if(nums[j] in count_map2):
+                    count_map2[nums[j]] += 1
+
+                else:
+                    count_map2[nums[j]] = 1
+
+                if(len(count_map1)==len(count_map2)):
+                    count += 1
+
+        return count                     
+                       
