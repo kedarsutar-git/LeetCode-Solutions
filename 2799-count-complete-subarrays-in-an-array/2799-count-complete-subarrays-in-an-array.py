622ad245c22ab93cca530a9aec2d53d6fcea51cb
kedar sutar <kedarsutar37@gmail.com>
2026-07-15 15:57:28 +0530

    Time: 5 ms (96.76%), Space: 19.6 MB (9.31%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -1,5 +1,43 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
+        totalDistinct = len(set(nums))
+
+        left = 0
+        right = 0
+        count = 0
+        freq = {}
+
+        while right < len(nums):
+
+            if nums[right] in freq:
+                freq[nums[right]] += 1
+            else:
+                freq[nums[right]] = 1
+
+            while len(freq) == totalDistinct:
+
+                # Every subarray starting at 'left' and ending at
+                # right, right+1, ..., n-1 will remain complete.
+                count += len(nums) - right
+
+                freq[nums[left]] -= 1
+
+                if freq[nums[left]] == 0:
+                    del freq[nums[left]]
+
+                left += 1
+
+            right += 1
+
+        return count
+
+
+
+
+
+'''
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
         count_map1 = {}
         for num in nums:
             if( num in count_map1):
@@ -25,4 +63,4 @@ class Solution:
 
         return count                     
                                        
-                       
+         '''              
