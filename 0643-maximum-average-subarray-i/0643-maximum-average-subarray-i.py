1ed4c2c015594edffd51ee7dba62c248dfab105c
kedar sutar <kedarsutar37@gmail.com>
2026-07-24 09:20:16 +0530

    Time: 143 ms (5.02%), Space: 29.2 MB (32.97%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,5 +1,30 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        right,left = 0,0
+        currentavg =  0
+        maxavg = float("-inf")
+
+        while(right<len(nums)):
+            currentavg += nums[right]
+            
+
+            if(right-left+1<k):
+                right += 1
+
+            elif(right-left+1==k):
+                maxavg = max(maxavg,currentavg/k)
+
+                currentavg -= nums[left]
+
+                left += 1
+                right += 1
+
+        return maxavg   
+'''
+
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
         window_sum = sum(nums[:k])
         Max_sum = window_sum
 
@@ -8,6 +33,6 @@ class Solution:
 
             Max_sum = max(Max_sum,window_sum)
 
-        return Max_sum/k    
-
-        
+        return Max_sum/k 
+    
+        '''
