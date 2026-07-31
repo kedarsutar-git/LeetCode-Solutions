53f9aa017bd1068d12f148ec4b0ae9d3981d2619
kedar sutar <kedarsutar37@gmail.com>
2026-07-31 20:45:13 +0530

    Time: 74 ms (90.77%), Space: 19.8 MB (75.3%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def balancedString(self, s: str) -> int:
+        target = len(s) // 4
+        count_map = defaultdict(int)
+
+        for char in s:
+            count_map[char] += 1
+
+        if (count_map["Q"] == target and count_map["W"] == target and count_map["E"] == target and count_map["R"] == target):
+            return 0
+
+        left = 0
+        minlen = len(s)
+
+        for right in range(len(s)):
+            count_map[s[right]] -= 1
+
+            while (left <= right and count_map["Q"] <= target and count_map["W"] <= target and count_map["E"] <= target and count_map["R"] <= target):
+
+                minlen = min(minlen, right - left + 1)
+                count_map[s[left]] += 1
+                left += 1
+
+        return minlen
