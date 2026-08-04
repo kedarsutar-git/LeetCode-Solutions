d55f83cff95bedafa622f3c84f2f41e53492b728
kedar sutar <kedarsutar37@gmail.com>
2026-08-04 20:06:08 +0530

    Time: 0 ms (100%), Space: 19.4 MB (24.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,20 +3,42 @@ class Solution:
         stack = []
         brac = {")":"(","]":"[","}":"{"}
 
-        for char in s:
-            if char in "([{":
+        for i in range(len(s)):
+            
+            char = s[i]
+            # if opening Bracket push in the Stack 
+            # if closing Bracket Stacktop check and pop
+
+            if(char=="(" or char=="{" or char=="["):  # for opening Bracket
                 stack.append(char)
+            
+            else:  # for closing Braket
+                if(len(stack)!=0):
+                    top = stack[-1]
 
-            else:
-                if not stack:
+                    if(char==")" and top =="(" or char=="}" and top=="{" or char=="]" and top=="[" ):
+                        stack.pop()
+
+                    else:
+                        return False
+                
+                else:
                     return False
+        
+        if(len(stack)==0):
+            return True 
+        
+        else:
+            return False
 
-                if(stack[-1]!=brac[char]):
-                    return False
+    
+        
 
-                stack.pop()
 
-        return len(stack)==0
+
+
+
+
               
         
 
