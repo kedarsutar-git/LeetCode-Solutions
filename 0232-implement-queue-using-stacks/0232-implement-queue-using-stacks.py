f57a69da13bbde8141cc95a4138451b16637acf9
kedar sutar <kedarsutar37@gmail.com>
2026-08-06 00:07:28 +0530

    Time: 0 ms (100%), Space: 19.6 MB (15.56%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,31 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self):
+        if self.empty():
+            return "Queue Underflow"
+
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        return self.stack2.pop()
+
+    def peek(self):
+        if self.empty():
+            return "Queue Underflow"
+
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        return self.stack2[-1]
+
+    def empty(self):
+        return len(self.stack1) == 0 and len(self.stack2) == 0
