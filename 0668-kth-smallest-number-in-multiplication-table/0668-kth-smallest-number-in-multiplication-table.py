2d781e576d11a85607bbbdae77641f68e08c9614
kedar sutar <kedarsutar37@gmail.com>
2026-08-19 00:52:58 +0530

    Time: 345 ms (26.44%), Space: 19.2 MB (68.23%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
@@ -17,3 +17,31 @@ class Solution:
         
         return start
         
+
+'''
+class Solution:
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        matrix = []
+        for i in range(1,n+1):
+            row = []
+            for j in range(1,m+1):
+                row.append(i*j)
+
+            matrix.append(row)
+
+        arr =[]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                arr.append(matrix[i][j])
+        
+        arr.sort()
+
+        count = 0
+        ans  = 0
+        for x in range(len(arr)):
+            count += 1
+            if(count==k):
+                ans  = arr[x]
+
+        return ans 
+'''
