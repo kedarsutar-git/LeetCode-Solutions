ece51c2bf24639cd5c88eec3806c280fb2aa53e1
kedar sutar <kedarsutar37@gmail.com>
2026-08-27 00:00:55 +0530

    Time: 2846 ms (5%), Space: 20.9 MB (87%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -3,13 +3,13 @@ class Solution:
         temp = [-1]*len(nums)
 
         for i in range(len(nums)):
-            for j in range(1,len(nums)):
-                index = (i+j)%len(nums)
+            for j in range(i+1,i+len(nums)):
+                index = j%len(nums)
 
                 if(nums[index]>nums[i]):
                     temp[i] = nums[index]
                     break
         return temp
 
-        
+
         
