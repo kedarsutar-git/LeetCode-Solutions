d63a24923d0f5ef8bc9919c0b86d5eb6393c9e18
kedar sutar <kedarsutar37@gmail.com>
2026-09-08 19:07:34 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6.67%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,5 +1,29 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
+        heap = []
+
+        for value in nums:
+            if value > 0:
+                heapq.heappush(heap, value)
+
+        operation = 0
+        previous = 0
+
+        while heap:
+            value = heapq.heappop(heap)
+
+            if value != previous:
+                operation += 1
+                previous = value
+
+        return operation
+
+
+
+'''
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
         opertion = 0
         while(sum(nums)!=0):
             for value in nums:
@@ -19,3 +43,4 @@ class Solution:
             opertion += 1
 
         return opertion
+'''
